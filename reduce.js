b7d188b290b6a7a31a86ea2f2572aae40d4cb6b1
dehmer <horst.dehmer@gmail.com>
2019-10-25 19:41:28 +0200

    feature construction (WIP).

--- reduce.js
+++ reduce.js
@@ -52,51 +52,72 @@ const replace = (position, s) => o => o.substring(0, position) + s + o.substring
 const sidc = async streams => {
   const { db, stream, filter, map, reduce, object, list } = streams
 
-  /*
-    'UNIT-TYPE' (10000081/10000129):
-        UNIT_TYPE_cat_code
-        UNIT_TYPE_arm_cat_code
-        coalesce(UNIT_TYPE_arm_spclsn_code, '[NULL]')
-        coalesce(UNIT_TYPE_suppl_spclsn_code, '[NULL]')
-        coalesce(UNIT_TYPE_gen_mob_code, '[NULL]')
-        coalesce(UNIT_TYPE_qual_code, '[NULL]')]
-  */
-
   const coalesce = value => value || '[NULL]'
   const mapper = {}
 
-  mapper[ent_id.UNIT_TYPE] = record => [0, 1, -2, -3, -4, -5].map(index => index < 0 ? coalesce(record[-index]) : record[index])
-  mapper[ent_id.FAC_TYPE] = record => [record[0]]
-  mapper[ent_id.CTRL_FEAT_TYPE] = record => [record[0]]
-  mapper[ent_id.HRBR_TYPE] = record => [record[0]]
+  mapper[ent_id.UNIT_TYPE] = value => {
+    const type = value[ent_id.OBJ_ITEM_TYPE][ent_id.UNIT_TYPE]
+    return [0, 1, -2, -3, -4, -5].map(index => index < 0 ? coalesce(type[-index]) : type[index])
+  }
+
+  mapper[ent_id.FAC_TYPE] = value => {
+    const type = value[ent_id.OBJ_ITEM_TYPE][ent_id.FAC_TYPE]
+    return [type[0]]
+  }
+
+  mapper[ent_id.CTRL_FEAT_TYPE] = value => {
+    const type = value[ent_id.OBJ_ITEM_TYPE][ent_id.CTRL_FEAT_TYPE]
+    return [type[0]]
+  }
+
+  mapper[ent_id.HRBR_TYPE] = value => {
+    const type = value[ent_id.OBJ_ITEM_TYPE][ent_id.HRBR_TYPE]
+    return [type[0]]
+  }
+
+  mapper[ent_id.ROUTE_TYPE] = value => {
+    const type = value[ent_id.OBJ_ITEM_TYPE][ent_id.ROUTE_TYPE]
+    const route = value[ent_id.OBJ_ITEM][ent_id.ROUTE]
+    if (route[0] && (type[0] === 'ALTSPL' || type[0] === 'MSR')) return [type[0], route[0]]
+    else return [type[0]]
+  }
+
+  mapper[ent_id.TASK_FRMTN_TYPE] = value => {
+    // Convoy is either moving or not.
+    // TODO: add 'MOVING'/'HALTED' to descr_txt
+    // "10000206:MLCNVY |" : "G*S*LCH---****X",
+    // "10000206:MLCNVY |" : "G*S*LCM---****X"
+    return []
+  }
 
   const type = data => data.value[ent_id.OBJ_ITEM_TYPE]
 
   const function_id = data => K(data)(data => {
+    data.properties = {}
     Object.entries(mapper)
       .filter(([key]) => data.value[ent_id.OBJ_ITEM_TYPE][key])
       .map(([key, fn]) => {
-        const descr_txt = fn(data.value[ent_id.OBJ_ITEM_TYPE][key]).join(' | ')
+        const descr_txt = fn(data.value).join(' | ')
         const sidc = symbol_mapping[`${key}:${descr_txt}`]
-        if (sidc) data.sidc = sidc
+        if (sidc) data.properties.sidc = sidc
       })
   })
 
   const standard_id = data => K(data)(data => {
-    if (!data.sidc) return
+    if (!data.properties.sidc) return
     if (!data.value[ent_id.OBJ_ITEM_HSTLY_STAT]) return
-    data.sidc = replace(1, ID[data.value[ent_id.OBJ_ITEM_HSTLY_STAT]] || 'U')(data.sidc)
+    data.properties.sidc = replace(1, ID[data.value[ent_id.OBJ_ITEM_HSTLY_STAT]] || 'U')(data.properties.sidc)
   })
 
   const echelon = data => K(data)(data => {
-    if (!data.sidc) return
+    if (!data.properties.sidc) return
     const records = data.value[ent_id.OBJ_ITEM_TYPE]
-    if (records[ent_id.UNIT_TYPE]) data.sidc = replace(10, ECHECLON[records[ent_id.UNIT_TYPE][6]] || '-')(data.sidc)
-    else if (records[ent_id.CTRL_FEAT_TYPE]) data.sidc = replace(10, ECHECLON[records[ent_id.CTRL_FEAT_TYPE][1]] || '-')(data.sidc)
+    if (records[ent_id.UNIT_TYPE]) data.properties.sidc = replace(10, ECHECLON[records[ent_id.UNIT_TYPE][6]] || '-')(data.properties.sidc)
+    else if (records[ent_id.CTRL_FEAT_TYPE]) data.properties.sidc = replace(10, ECHECLON[records[ent_id.CTRL_FEAT_TYPE][1]] || '-')(data.properties.sidc)
   })
 
   const status = data => K(data)(data => {
-    if (!data.sidc) return
+    if (!data.properties.sidc) return
     if (!data.value[ent_id.RPTD]) return
     const codes = {
       ASS: 'A',
@@ -107,12 +128,47 @@ const sidc = async streams => {
     }
 
     const code = data.value[ent_id.RPTD][1]
-    data.sidc = replace(3, codes[code] || '-')(data.sidc)
+    data.properties.sidc = replace(3, codes[code] || '-')(data.properties.sidc)
   })
 
-  const xs = await R.compose(
+  const geometry = data => K(data)(data => {
+    if (!data.value[ent_id.OBJ_ITEM_LOC]) return
+
+    const record = data.value[ent_id.OBJ_ITEM_LOC]
+    data.geometry = {}
+    data.geometry.type = data.value[ent_id.OBJ_ITEM_LOC].type
+    data.geometry.coordinates = data.value[ent_id.OBJ_ITEM_LOC].coordinates
+
+    if (record.geometry_mnm_range)    data.properties.geometry_mnm_range    = record.geometry_mnm_range
+    if (record.geometry_max_range)    data.properties.geometry_max_range    = record.geometry_max_range
+    if (record.geometry_orient_angle) data.properties.geometry_orient_angle = record.geometry_orient_angle
+    if (record.geometry_size_angle)   data.properties.geometry_size_angle   = record.geometry_size_angle
+    if (record.geometry_width)        data.properties.geometry_width        = record.geometry_width
+    if (record.brng_angle)            data.properties.q                     = record.brng_angle
+    if (record.speed_rate)            data.properties.z                     = record.speed_rate
+  })
+
+  const properties = data => K(data)(data => {
+    data.properties.t = data.value[ent_id.OBJ_ITEM][ent_id.OBJ_ITEM][1]
+  })
+
+  const feature = data => ({
+    type: 'Feature',
+    geometry: data.geometry,
+    properties: data.properties
+  })
+
+  const validFeature = feature => {
+    if (!feature.geometry) return false
+    return true
+  }
+
+  const features = await R.compose(
     list,
-    map(data => data.sidc),
+    filter(validFeature),
+    map(feature),
+    map(properties),
+    map(geometry),
     map(status),
     map(echelon),
     map(standard_id),
@@ -120,7 +176,11 @@ const sidc = async streams => {
     filter(type),
     stream
   )(`TARGET:${ent_id.CONTXT_OI_ASSOC}`)
-  console.log(xs)
+
+  console.log(JSON.stringify({
+    type: 'FeatureCollection',
+    features: features
+  }, null, 2))
 }
 
 const ready = []
